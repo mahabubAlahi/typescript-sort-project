4c755081667c70890bbe09dcf9f8d3036e3a440d
Mahabub Alahi <mahabubalahi9531@gmail.com>
2022-06-30 12:58:28 +0600

    add(wip): sorter class implemented

--- build/index.js
+++ build/index.js
@@ -1,2 +1,30 @@
 "use strict";
-console.log('Hi There');
+class Sorter {
+    constructor(collection) {
+        this.collection = collection;
+    }
+    sort() {
+        let { length } = this.collection;
+        let i = 0;
+        while (i < length - 1) {
+            let j = length;
+            let start = 0;
+            while (start < j) {
+                if (this.collection instanceof Array) {
+                    if (this.collection[start] > this.collection[start + 1]) {
+                        let temp = this.collection[start];
+                        this.collection[start] = this.collection[start + 1];
+                        this.collection[start + 1] = temp;
+                    }
+                    start++;
+                }
+                if (typeof this.collection === 'string') {
+                }
+            }
+            length = length - 1;
+        }
+    }
+}
+const sorter = new Sorter([5, 1, 2, 90, 45, -7]);
+sorter.sort();
+console.log(sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,44 @@
-console.log('Hi There')
+
+class Sorter {
+
+    constructor(public collection: number[] | string) { }
+
+    //Bubble Sort
+    sort(): void {
+        let { length } = this.collection;
+
+        let i = 0;
+
+        while (i < length - 1) {
+
+            let j = length
+            let start = 0
+
+            while (start < j) {
+
+                if (this.collection instanceof Array) {
+
+                    if (this.collection[start] > this.collection[start + 1]) {
+                        let temp = this.collection[start];
+                        this.collection[start] = this.collection[start + 1];
+                        this.collection[start + 1] = temp;
+                    }
+                    start++;
+                }
+
+                if(typeof this.collection === 'string'){
+
+                }
+
+            }
+            length = length - 1;
+        }
+
+    }
+}
+
+const sorter = new Sorter([5, 1, 2, 90, 45, -7]);
+
+sorter.sort()
+
+console.log(sorter.collection)

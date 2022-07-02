3c2e6d980d890c38834bdf5ddf318edce5e23a43
Mahabub Alahi <mahabubalahi9531@gmail.com>
2022-07-02 19:15:11 +0600

    add: linked list sorting implementation

---
+++ build/LinkedList.js
@@ -0,0 +1,79 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LinkedList = void 0;
+const Sorter_1 = require("./Sorter");
+class NodeLinkedList {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+class LinkedList extends Sorter_1.Sorter {
+    constructor() {
+        super(...arguments);
+        this.head = null;
+    }
+    add(data) {
+        const node = new NodeLinkedList(data);
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+        let tail = this.head;
+        while (tail.next) {
+            tail = tail.next;
+        }
+        tail.next = node;
+    }
+    get length() {
+        if (!this.head)
+            return 0;
+        let tail = this.head;
+        let counter = 0;
+        while (tail.next) {
+            tail = tail.next;
+            counter++;
+        }
+        counter++;
+        return counter;
+    }
+    at(index) {
+        if (!this.head) {
+            throw new Error('Index out of bounds');
+        }
+        let counter = 0;
+        let node = this.head;
+        while (node) {
+            if (counter === index) {
+                return node;
+            }
+            counter++;
+            node = node.next;
+        }
+        throw new Error('Index out of bounds');
+    }
+    compare(leftIndex, rightIndex) {
+        if (!this.head) {
+            throw new Error('List is empty');
+        }
+        return this.at(leftIndex).data > this.at(rightIndex).data;
+    }
+    swap(leftIndex, rightIndex) {
+        let node1 = this.at(leftIndex);
+        let node2 = this.at(rightIndex);
+        const leftHand = node1.data;
+        node1.data = node2.data;
+        node2.data = leftHand;
+    }
+    print() {
+        if (!this.head) {
+            return;
+        }
+        let node = this.head;
+        while (node) {
+            console.log(node.data);
+            node = node.next;
+        }
+    }
+}
+exports.LinkedList = LinkedList;
---
+++ src/LinkedList.ts
@@ -0,0 +1,100 @@
+import { Sorter } from "./Sorter";
+
+class NodeLinkedList {
+    
+    next: NodeLinkedList | null = null;
+
+    constructor(public data: number){}
+}
+
+export class LinkedList extends Sorter {
+    head: NodeLinkedList | null = null;
+
+    add(data: number): void {
+        const node = new NodeLinkedList(data);
+
+        if(!this.head){
+            this.head = node;
+            return;
+        }
+
+        let tail = this.head;
+
+        while(tail.next){
+            tail = tail.next
+        }
+
+        tail.next = node;
+    }
+
+    get length(): number {
+        if(!this.head)
+            return 0;
+
+        let tail = this.head;
+        let counter = 0;
+
+        while(tail.next){
+            tail = tail.next
+            counter++;
+        }
+
+        counter++;
+
+        return counter;
+    }
+
+    at(index: number): NodeLinkedList{
+        if(!this.head){
+            throw new Error('Index out of bounds');
+        }
+
+        let counter = 0;
+        let node: NodeLinkedList | null = this.head;
+
+        while(node){
+
+            if(counter === index){
+                return node;
+            }
+
+            counter++;
+
+            node = node.next;
+        }
+
+        throw new Error('Index out of bounds');
+         
+    }
+
+    compare(leftIndex: number, rightIndex: number): boolean {
+        if(!this.head){
+            throw new Error('List is empty');
+        }
+
+        return this.at(leftIndex).data > this.at(rightIndex).data;
+    }
+
+    swap(leftIndex: number, rightIndex: number): void {
+        let node1 = this.at(leftIndex);
+        let node2 = this.at(rightIndex);
+
+        const leftHand = node1.data;
+        node1.data = node2.data;
+        node2.data = leftHand;
+
+    }
+
+    print(): void {
+        if(!this.head){
+            return;
+        }
+
+        let node: NodeLinkedList | null = this.head;
+
+        while(node){
+            console.log(node.data);
+            node = node.next;
+        }
+    }
+}

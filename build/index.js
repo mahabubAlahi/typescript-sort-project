409daeb76f7614826ffc0eabed0df6bbd621b667
Mahabub Alahi <mahabubalahi9531@gmail.com>
2022-07-02 19:22:06 +0600

    refactor: number collection, character collection and linked list collection function call updated

--- build/index.js
+++ build/index.js
@@ -1,12 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const CharactersCollection_1 = require("./CharactersCollection");
-const Sorter_1 = require("./Sorter");
-// const numbersCollection = new NumbersCollection([5, 1, 2, 90, 45, -7, 90000, -4545454]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort()
-// console.log(numbersCollection.data)
+const LinkedList_1 = require("./LinkedList");
+const NumbersCollection_1 = require("./NumbersCollection");
+const numbersCollection = new NumbersCollection_1.NumbersCollection([5, 1, 2, 90, 45, -7, 90000, -4545454]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 const charactersCollection = new CharactersCollection_1.CharactersCollection('Xaayb');
-const sorter = new Sorter_1.Sorter(charactersCollection);
-sorter.sort();
+charactersCollection.sort();
 console.log(charactersCollection.data);
+const linkedList = new LinkedList_1.LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+linkedList.sort();
+linkedList.print();
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,27 @@
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 import { NumbersCollection } from './NumbersCollection';
 import { Sorter } from './Sorter'
 
 
-// const numbersCollection = new NumbersCollection([5, 1, 2, 90, 45, -7, 90000, -4545454]);
-// const sorter = new Sorter(numbersCollection);
+const numbersCollection = new NumbersCollection([5, 1, 2, 90, 45, -7, 90000, -4545454]);
 
-// sorter.sort()
+numbersCollection.sort()
 
-// console.log(numbersCollection.data)
+console.log(numbersCollection.data)
 
 const charactersCollection = new CharactersCollection('Xaayb');
-const sorter = new Sorter(charactersCollection);
 
-sorter.sort();
+charactersCollection.sort();
 
 console.log(charactersCollection.data)
+
+const linkedList = new LinkedList();
+
+linkedList.add(500);
+linkedList.add(-10)
+linkedList.add(-3);
+linkedList.add(4);
+
+linkedList.sort();
+linkedList.print()
